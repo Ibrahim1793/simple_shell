64225a5ba46b085f0b8664875d4fb08bc9f241e3
Brucekola <brucechilenga24@gmail.com>
2021-08-25 08:41:39 -0700

    Betty would be proud

--- pass_betty.c
+++ pass_betty.c
@@ -12,6 +12,13 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+/**
+ * _islower - prints if char is lower case
+ * @c: The char to print
+ * Return: on true 1
+ * On false 0.
+ */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
@@ -29,14 +36,13 @@ int _islower(int c)
  */
 int main(void)
 {
-    int r;
-
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+int r;
+r = _islower('H');
+_putchar(r + '0');
+r = _islower('o');
+_putchar(r + '0');
+r = _islower(108);
+_putchar(r + '0');
+_putchar('\n');
+return (0);
 }
